8251d913b7dee643e4b443ab6c319cabb8cf3432
lumdy690 <dyrellumiwes@gmail.com>
2023-07-29 16:16:49 +1200

    Cards done

--- birbs.js
+++ birbs.js
@@ -56,6 +56,7 @@ async function populate() {
       const weight = document.createElement("p");
 
 
+      //Giving ID's
       imageArticle.setAttribute("class", "birb-image-article");
       colourCircle.setAttribute("class", "birb-colour-circle");
 
@@ -63,6 +64,7 @@ async function populate() {
       photo.setAttribute("class", "birb-photo");
       credit.setAttribute("class", "birb-credit");
       primaryName.setAttribute("class", "birb-primary-name");
+      
       engName.setAttribute("class", "birb-english-name");
       sciName.setAttribute("class", "birb-scientific-name");
       order.setAttribute("class", "birb-order");
@@ -72,17 +74,12 @@ async function populate() {
       //size.setAttribute("class", "birb-size");
       length.setAttribute("class", "birb-length");
       weight.setAttribute("class", "birb-weight");
-
-      const sciNameLabel = document.createElement("p");
-      sciNameLabel.textContent = "Scientific Name";
-      sciName.appendChild(sciNameLabel);
+      //Trial
     
-
-
-  
+    
       primaryName.textContent = birb.primary_name;
       engName.textContent = birb.english_name;
-      sciName.textContent =birb.scientific_name;
+      sciName.textContent = birb.scientific_name;
       order.textContent = birb.order;
       family.textContent = birb.family;
       status.textContent = birb.status;
@@ -93,7 +90,7 @@ async function populate() {
         otherNames.appendChild(li);
       }
       photo.src = birb.photo.source;
-      credit.textContent = birb.photo.credit;
+      credit.textContent ="Photo by " + birb.photo.credit;
       length.textContent = birb.size.length.value + " " + birb.size.length.units;
       weight.textContent = birb.size.weight.value + " " + birb.size.weight.units;
       //size.appendChild(length);
@@ -101,27 +98,101 @@ async function populate() {
   
       //try image by itself
       //imageArticle.appendChild(photo);
+      /*imageArticle.appendChild(primaryName)
+      imageArticle.appendChild(colourCircle)
+      imageArticle.appendChild(credit)*/
+
+    if (birb.status.toLowerCase().localeCompare("Not Threatened") === 0) {
+        colourCircle.style.backgroundColor = "#02a028";
+    }
+    else if (birb.status.toLowerCase().localeCompare("naturally uncommon") === 0) {
+      colourCircle.style.backgroundColor = "#649a31";
+    }
+    else if (birb.status.toLowerCase().localeCompare("relict") === 0) {
+      colourCircle.style.backgroundColor = "#99cb68";
+    }
+    else if (birb.status.toLowerCase().localeCompare("recovering") === 0) {
+      colourCircle.style.backgroundColor = "#02a028";
+    }
+    else if (birb.status.toLowerCase().localeCompare("declining") === 0) {
+      colourCircle.style.backgroundColor = "#fe9a01";
+    }
+    else if (birb.status.toLowerCase().localeCompare("nationally increasing") === 0) {
+        colourCircle.style.backgroundColor = "#c26967";
+    }
+    else if (birb.status.toLowerCase().localeCompare("nationally vulnerable") === 0) {
+      colourCircle.style.backgroundColor = "#9b0000";
+    }
+    else if (birb.status.toLowerCase().localeCompare("nationally endangered") === 0) {
+      colourCircle.style.backgroundColor = "#660032";
+    }
+    else if (birb.status.toLowerCase().localeCompare("nationally critical") === 0) {
+      colourCircle.style.backgroundColor = "#320033";
+    }
+    else if (birb.status.toLowerCase().localeCompare("extinct") === 0) {
+      colourCircle.style.backgroundColor = "black";
+    }
+    else if (birb.status.toLowerCase().localeCompare("data deficient") === 0) {
+      colourCircle.style.backgroundColor = "black";
+    }else {
+    colourCircle.style.backgroundColor = "#02a028";
+    }
+
+    const labels = document.createElement("div");
+    labels.setAttribute("class", "birb-labels");
+
+
+    const sciNameLabel = document.createElement("p");
+    sciNameLabel.textContent = "Scientific Name: ";
+    sciName.appendChild(sciNameLabel);
+ 
+    const orderLabel = document.createElement("p");
+    orderLabel.textContent = "Order:";
+    const familyLabel = document.createElement("p");
+    familyLabel.textContent = "Family:";
+
+    const statusLabel = document.createElement("p");
+    statusLabel.textContent = "Status:";
+
+    const lengthLabel = document.createElement("p");
+    lengthLabel.textContent = "Length:";
+    const weightLabel = document.createElement("p");
+    weightLabel.textContent = "Weight";
+
+
+
+
+    labels.appendChild(sciNameLabel);
+    labels.appendChild(orderLabel);
+    labels.appendChild(familyLabel);
+    labels.appendChild(statusLabel);
+    labels.appendChild(lengthLabel);
+    labels.appendChild(weightLabel);
+
+
+
       myArticle.appendChild(photo);
       myArticle.appendChild(primaryName);
       myArticle.appendChild(credit);
-
-
-      
-
       myArticle.appendChild(colourCircle);
+
       myArticle.appendChild(engName);
       myArticle.appendChild(sciName);
       myArticle.appendChild(order);
       myArticle.appendChild(family);
       //myArticle.appendChild(otherNames);
       myArticle.appendChild(status);
-      
       myArticle.appendChild(length);
       myArticle.appendChild(weight);
 
       //Image and article are children of section
-      section.appendChild(imageArticle);
+     // section.appendChild(imageArticle);
       section.appendChild(myArticle);
+      section.appendChild(labels);
+
+      //myArticle.append(imageArticle);
+
+      
 
     }
 
